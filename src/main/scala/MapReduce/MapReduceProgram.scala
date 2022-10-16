6dedb36ce711b5e5bcd851241da5996614c7932d
Nikita Mashchenko <mashchenko1@icloud.com>
2022-10-16 00:32:28 -0500

    part1 done

--- src/main/scala/MapReduce/MapReduceProgram.scala
+++ src/main/scala/MapReduce/MapReduceProgram.scala
@@ -7,16 +7,21 @@ import collection.JavaConverters.asScalaIteratorConverter
 //import jdk.CollectionConverters.IteratorHasAsScala
 import java.io.IOException
 import java.util
+import com.typesafe.config.{Config, ConfigFactory}
 
 object MapReduceProgram:
   class Map extends MapReduceBase with Mapper[LongWritable, Text, Text, IntWritable] :
     private final val one = new IntWritable(1)
     private val word = new Text()
+    val config: Config = ConfigFactory.load("application.conf").getConfig("randomLogGenerator")
 
     @throws[IOException]
     def map(key: LongWritable, value: Text, output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
       val line: String = value.toString
-      line.split(" ").foreach { token =>
+      if (config.getString("Pattern").r.findAllIn(line).nonEmpty == true) {
+        val time = line.split(" ")(0).substring(0, 5)
+        val logType = line.split(" ")(2)
+        val token = time + ", " + logType
         word.set(token)
         output.collect(word, one)
       }
@@ -32,6 +37,7 @@ object MapReduceProgram:
     conf.set("fs.defaultFS", "local")
     conf.set("mapreduce.job.maps", "1")
     conf.set("mapreduce.job.reduces", "1")
+    conf.set("mapred.textoutputformat.separator", ", ")
     conf.setOutputKeyClass(classOf[Text])
     conf.setOutputValueClass(classOf[IntWritable])
     conf.setMapperClass(classOf[Map])
